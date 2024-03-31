6f49d962956372d3b046de9bec6c8ec9e4a015fd
Art-P-777 <dvayder-19@mail.ru>
2024-03-31 09:05:10 +0400

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form2.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            splitContainer1 = new SplitContainer();
+            coef_c = new TextBox();
+            coef_b = new TextBox();
+            coef_a = new TextBox();
+            About_btn = new Button();
+            Reset_btn = new Button();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Margin = new Padding(5, 0, 5, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(coef_c);
+            splitContainer1.Panel1.Controls.Add(coef_b);
+            splitContainer1.Panel1.Controls.Add(coef_a);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(About_btn);
+            splitContainer1.Panel2.Controls.Add(Reset_btn);
+            splitContainer1.Panel2.Controls.Add(button1);
+            splitContainer1.Size = new Size(398, 216);
+            splitContainer1.SplitterDistance = 135;
+            splitContainer1.SplitterWidth = 1;
+            splitContainer1.TabIndex = 0;
+            // 
+            // coef_c
+            // 
+            coef_c.Dock = DockStyle.Top;
+            coef_c.Font = new Font("Segoe UI", 15F);
+            coef_c.Location = new Point(0, 94);
+            coef_c.Name = "coef_c";
+            coef_c.PlaceholderText = "c :";
+            coef_c.Size = new Size(398, 47);
+            coef_c.TabIndex = 2;
+            coef_c.KeyPress += coef_KeyPress;
+            // 
+            // coef_b
+            // 
+            coef_b.Dock = DockStyle.Top;
+            coef_b.Font = new Font("Segoe UI", 15F);
+            coef_b.Location = new Point(0, 47);
+            coef_b.Name = "coef_b";
+            coef_b.PlaceholderText = "b :";
+            coef_b.Size = new Size(398, 47);
+            coef_b.TabIndex = 1;
+            coef_b.KeyPress += coef_KeyPress;
+            // 
+            // coef_a
+            // 
+            coef_a.Dock = DockStyle.Top;
+            coef_a.Font = new Font("Segoe UI", 15F);
+            coef_a.Location = new Point(0, 0);
+            coef_a.Name = "coef_a";
+            coef_a.PlaceholderText = "a :";
+            coef_a.Size = new Size(398, 47);
+            coef_a.TabIndex = 0;
+            coef_a.KeyPress += coef_KeyPress;
+            // 
+            // About_btn
+            // 
+            About_btn.Image = (Image)resources.GetObject("About_btn.Image");
+            About_btn.Location = new Point(307, 0);
+            About_btn.Name = "About_btn";
+            About_btn.Size = new Size(91, 80);
+            About_btn.TabIndex = 2;
+            About_btn.UseVisualStyleBackColor = true;
+            About_btn.Click += About_btn_Click;
+            // 
+            // Reset_btn
+            // 
+            Reset_btn.Font = new Font("Segoe UI", 1F);
+            Reset_btn.Image = (Image)resources.GetObject("Reset_btn.Image");
+            Reset_btn.Location = new Point(208, 0);
+            Reset_btn.Name = "Reset_btn";
+            Reset_btn.Size = new Size(105, 80);
+            Reset_btn.TabIndex = 1;
+            Reset_btn.UseVisualStyleBackColor = true;
+            Reset_btn.Click += Reset_btn_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold | FontStyle.Italic);
+            button1.Location = new Point(0, 0);
+            button1.Margin = new Padding(5, 0, 5, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(210, 80);
+            button1.TabIndex = 0;
+            button1.Text = "Решить уравнение";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AllowDrop = true;
+            AutoScaleMode = AutoScaleMode.None;
+            ClientSize = new Size(398, 216);
+            Controls.Add(splitContainer1);
+            Font = new Font("Segoe UI", 9F);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            KeyPreview = true;
+            Margin = new Padding(5, 0, 5, 0);
+            MaximizeBox = false;
+            Name = "Form2";
+            ShowIcon = false;
+            Text = "Решатель квадратных уравнений";
+            TopMost = true;
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel1.PerformLayout();
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private SplitContainer splitContainer1;
+        private Button button1;
+        private TextBox coef_c;
+        private TextBox coef_b;
+        private TextBox coef_a;
+        private Button About_btn;
+        private Button Reset_btn;
+    }
+}
---
+++ WinFormsApp2/Form2.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void coef_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back && e.KeyChar != '-')
+                e.Handled = true;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(coef_a.Text);
+            double b = Convert.ToDouble(coef_b.Text);
+            double c = Convert.ToDouble(coef_c.Text);
+            double temp = (b * b - 4 * a * c);
+            if (temp < 0)
+                await Task.Run(() => MessageBox.Show("Дискриминант меньше нуля. Нет корней в зоне действительных чисел"));
+            else if (temp == 0)
+                await Task.Run(() => MessageBox.Show($"D = {temp} ; Значит один корень\nx = {(-1 * b) / 2 * a}"));
+            else
+                await Task.Run(() => MessageBox.Show($"D = {temp}\n" +
+                                               $"x1 = {(-1 * (b + Math.Pow(temp, 0.5))) / 2 * a}\n" +
+                                             $"x2 = {(-1 * (b - Math.Pow(temp, 0.5))) / 2 * a}\n",
+                                           "Решатель квадратных уравнений"));
+
+        }
+
+        private void Reset_btn_Click(object sender, EventArgs e)
+        {
+            coef_a.ResetText();
+            coef_b.ResetText();
+            coef_c.ResetText();
+        }
+
+        private async void About_btn_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>MessageBox.Show(
+                "ax^2 + bx + c = 0\n\n" +
+                "D = b^2 - 4ac\n\n" +
+                "        -b - D^0.5\n" +
+                "x1 = ------------\n" +
+                "            2a\n\n" +
+                "        -b + D^0.5\n" +
+                "x2 = -------------\n" +
+                "            2a\n\n\n" +
+                "Примечание: D^0.5 это корень! Степень числа 0.5 = арифметическому корню!"
+                ,"Квадратное уравнение"));
+        }
+    }
+
+    public static class HighDpiHelper
+    {
+        public static void AdjustControlImagesDpiScale(Control container)
+        {
+            var dpiScale = GetDpiScale(container).Value;
+            if (CloseToOne(dpiScale))
+                return;
+
+            AdjustControlImagesDpiScale(container.Controls, dpiScale);
+        }
+
+        private static void AdjustButtonImageDpiScale(ButtonBase button, float dpiScale)
+        {
+            var image = button.Image;
+            if (image == null)
+                return;
+
+            button.Image = ScaleImage(image, dpiScale);
+        }
+
+        private static void AdjustControlImagesDpiScale(Control.ControlCollection controls, float dpiScale)
+        {
+            foreach (Control control in controls)
+            {
+                var button = control as ButtonBase;
+                if (button != null)
+                    AdjustButtonImageDpiScale(button, dpiScale);
+                else
+                {
+                    var pictureBox = control as PictureBox;
+                    if (pictureBox != null)
+                        AdjustPictureBoxDpiScale(pictureBox, dpiScale);
+                }
+
+                AdjustControlImagesDpiScale(control.Controls, dpiScale);
+            }
+        }
+
+        private static void AdjustPictureBoxDpiScale(PictureBox pictureBox, float dpiScale)
+        {
+            var image = pictureBox.Image;
+            if (image == null)
+                return;
+
+            if (pictureBox.SizeMode == PictureBoxSizeMode.CenterImage)
+                pictureBox.Image = ScaleImage(pictureBox.Image, dpiScale);
+        }
+
+        private static bool CloseToOne(float dpiScale)
+        {
+            return Math.Abs(dpiScale - 1) < 0.001;
+        }
+
+        private static Lazy<float> GetDpiScale(Control control)
+        {
+            return new Lazy<float>(() =>
+            {
+                using (var graphics = control.CreateGraphics())
+                    return graphics.DpiX / 96.0f;
+            });
+        }
+
+        private static Image ScaleImage(Image image, float dpiScale)
+        {
+            var newSize = ScaleSize(image.Size, dpiScale);
+            var newBitmap = new Bitmap(newSize.Width, newSize.Height);
+
+            using (var g = Graphics.FromImage(newBitmap))
+            {
+                // According to this blog post http://blogs.msdn.com/b/visualstudio/archive/2014/03/19/improving-high-dpi-support-for-visual-studio-2013.aspx
+                // NearestNeighbor is more adapted for 200% and 200%+ DPI
+
+                var interpolationMode = InterpolationMode.HighQualityBicubic;
+                if (dpiScale >= 2.0f)
+                    interpolationMode = InterpolationMode.NearestNeighbor;
+
+                g.InterpolationMode = interpolationMode;
+                g.DrawImage(image, new Rectangle(new Point(), newSize));
+            }
+
+            return newBitmap;
+        }
+
+        private static Size ScaleSize(Size size, float scale)
+        {
+            return new Size((int)(size.Width * scale), (int)(size.Height * scale));
+        }
+    }
+}
